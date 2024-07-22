279b31c2ca3b71399581c8e105799b9bc2c1dd3e
Anton Konyk <antonkonyk.com>
2024-07-22 20:49:47 +0200

    add user, rest_framework to INSTALLED_APPS, set AUTH_USER_MODEL

--- train_station_service/settings.py
+++ train_station_service/settings.py
@@ -42,7 +42,11 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "station",
+    "user",
+    "rest_framework",
 ]
+
+AUTH_USER_MODEL = "user.User"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
