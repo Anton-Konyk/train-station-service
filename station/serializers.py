66328eb6fbee6462f16050f82cdcb14dedaf81b6
Anton Konyk <antonkonyk.com>
2024-07-23 20:19:25 +0200

    create TrainTypeSerializer, TrainTypeImageSerializer

--- station/serializers.py
+++ station/serializers.py
@@ -1,11 +1,23 @@
 from rest_framework import serializers
 
-from station.models import Crew
+from station.models import Crew, TrainType
 
 
 class CrewSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Crew
-        fields = ("first_name", "last_name")
+        fields = ("id", "first_name", "last_name")
 
+
+class TrainTypeSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = TrainType
+        fields = ("id", "name")
+
+
+class TrainTypeImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TrainType
+        fields = ("id", "image")
