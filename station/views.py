fad31941c047b8db1f3685129dce2dde906e5d9a
Anton Konyk <antonkonyk.com>
2024-07-23 19:15:15 +0200

    create CrewViewSet

--- station/views.py
+++ station/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from station.models import Crew
+from train_station_service.serializers import CrewSerializer
+
+
+class CrewViewSet(viewsets.ModelViewSet):
+    queryset = Crew.objects.all()
+    serializer_class = CrewSerializer
