ffabf125d723150462e2ce003ec675e516db7880
Anton Konyk <antonkonyk.com>
2024-07-24 20:53:03 +0200

    create endpoints for FacilityViewSet, TrainTypeViewSet, TrainViewSet

--- station/urls.py
+++ station/urls.py
@@ -1,13 +1,20 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from station.views import CrewViewSet
+from station.views import (
+    CrewViewSet,
+    FacilityViewSet,
+    TrainTypeViewSet,
+    TrainViewSet,
+)
 
 app_name = "station"
 
 router = routers.DefaultRouter()
 router.register("crews", CrewViewSet)
-
+router.register("facilities", FacilityViewSet)
+router.register("train_types", TrainTypeViewSet)
+router.register("trains", TrainViewSet)
 
 urlpatterns = [
     path("", include(router.urls))
