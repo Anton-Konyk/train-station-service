f300a06b4e84c423c54d20443662ceef2dda1192
Anton Konyk <antonkonyk.com>
2024-07-24 20:44:59 +0200

    create FacilitySerializer, TrainSerializer, TrainListSerializer, TrainRetrieveSerializer, add field image for TrainTypeSerializer

--- station/serializers.py
+++ station/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from station.models import Crew, TrainType
+from station.models import Crew, TrainType, Train, Facility
 
 
 class CrewSerializer(serializers.ModelSerializer):
@@ -10,14 +10,54 @@ class CrewSerializer(serializers.ModelSerializer):
         fields = ("id", "first_name", "last_name")
 
 
+class FacilitySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Facility
+        fields = ("id", "name")
+
+
 class TrainTypeSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = TrainType
-        fields = ("id", "name")
+        fields = ("id", "name", "image")
 
 
 class TrainTypeImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = TrainType
         fields = ("id", "image")
+
+
+class TrainSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Train
+        fields = (
+            "id",
+            "number",
+            "cargo_num",
+            "places_in_cargo",
+            "is_small",
+            "train_type",
+            "facilities"
+        )
+
+
+class TrainListSerializer(TrainSerializer):
+    train_type = serializers.SlugRelatedField(
+        read_only=True,
+        many=False,
+        slug_field="name"
+    )
+    facilities = serializers.SlugRelatedField(
+        many=True,
+        read_only=True,
+        slug_field="name"
+    )
+
+
+class TrainRetrieveSerializer(TrainSerializer):
+    train_type = TrainTypeSerializer(many=False)
+    facilities = FacilitySerializer(many=True)
