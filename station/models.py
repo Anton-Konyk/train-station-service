01604e0278599ac542ca0306925bd646a189bc5c
Anton Konyk <antonkonyk.com>
2024-07-27 15:23:13 +0200

    create validate_ticket

--- station/models.py
+++ station/models.py
@@ -168,42 +168,42 @@ class Ticket(models.Model):
         Order, on_delete=models.CASCADE, related_name="tickets"
     )
 
-    # @staticmethod
-    # def validate_ticket(row, seat, cinema_hall, error_to_raise):
-    #     for ticket_attr_value, ticket_attr_name, cinema_hall_attr_name in [
-    #         (row, "row", "rows"),
-    #         (seat, "seat", "seats_in_row"),
-    #     ]:
-    #         count_attrs = getattr(cinema_hall, cinema_hall_attr_name)
-    #         if not (1 <= ticket_attr_value <= count_attrs):
-    #             raise error_to_raise(
-    #                 {
-    #                     ticket_attr_name: f"{ticket_attr_name} "
-    #                     f"number must be in available range: "
-    #                     f"(1, {cinema_hall_attr_name}): "
-    #                     f"(1, {count_attrs})"
-    #                 }
-    #             )
-    #
-    # def clean(self):
-    #     Ticket.validate_ticket(
-    #         self.row,
-    #         self.seat,
-    #         self.movie_session.cinema_hall,
-    #         ValidationError,
-    #     )
-    #
-    # def save(
-    #     self,
-    #     force_insert=False,
-    #     force_update=False,
-    #     using=None,
-    #     update_fields=None,
-    # ):
-    #     self.full_clean()
-    #     return super(Ticket, self).save(
-    #         force_insert, force_update, using, update_fields
-    #     )
+    @staticmethod
+    def validate_ticket(cargo, seat, train, error_to_raise):
+        for ticket_attr_value, ticket_attr_name, train_attr_name in [
+            (cargo, "cargo", "cargo_num"),
+            (seat, "seat", "places_in_cargo"),
+        ]:
+            count_attrs = getattr(train, train_attr_name)
+            if not (1 <= ticket_attr_value <= count_attrs):
+                raise error_to_raise(
+                    {
+                        ticket_attr_name: f"{ticket_attr_name} "
+                        f"number must be in available range: "
+                        f"(1, {train_attr_name}): "
+                        f"(1, {count_attrs})"
+                    }
+                )
+
+    def clean(self):
+        Ticket.validate_ticket(
+            self.cargo,
+            self.seat,
+            self.journey.train,
+            ValidationError,
+        )
+
+    def save(
+        self,
+        force_insert=False,
+        force_update=False,
+        using=None,
+        update_fields=None,
+    ):
+        self.full_clean()
+        return super(Ticket, self).save(
+            force_insert, force_update, using, update_fields
+        )
 
     def __str__(self):
         return (
