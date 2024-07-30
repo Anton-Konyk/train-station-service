ed39c53ab9139c25ef624c3bdafee7fa48d7c49d
Anton Konyk <antonkonyk.com>
2024-07-30 18:25:54 +0200

    create pathes for SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView

--- train_station_service/urls.py
+++ train_station_service/urls.py
@@ -17,6 +17,11 @@ Including another URLconf
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularSwaggerView,
+    SpectacularRedocView, SpectacularAPIView
+)
+
 from train_station_service import settings
 
 
@@ -24,4 +29,15 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/station/", include("station.urls"), name="station"),
     path("api/v1/user/", include("user.urls", namespace="user")),
+    path("api/v1/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+         "api/v1/doc/swagger/",
+         SpectacularSwaggerView.as_view(url_name="schema"),
+         name="swagger-ui"
+         ),
+    path(
+         "api/v1/doc/redoc/",
+         SpectacularRedocView.as_view(url_name="schema"),
+         name="redoc"
+    ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
