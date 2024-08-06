621226cee7f8317a7d4d3295eb927fdda41bf073
Anton Konyk <antonkonyk.com>
2024-08-06 17:14:09 +0200

    change DATABASES settings for postgresql database working

--- train_station_service/settings.py
+++ train_station_service/settings.py
@@ -84,17 +84,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "train_station_service.wsgi.application"
 
-
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ["POSTGRES_DB"],
+        "USER": os.environ["POSTGRES_USER"],
+        "PASSWORD": os.environ["POSTGRES_PASSWORD"],
+        "HOST": os.environ["POSTGRES_HOST"],
+        "PORT": os.environ["POSTGRES_PORT"],
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
